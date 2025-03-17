b17c9917a0e8397cae4116243d6d7cbd524e1f9d
Iñigo Solano <i@errstate.com>
2025-03-17 20:13:13 +0100

    add telescope for Git files

--- init.lua
+++ init.lua
@@ -344,6 +344,8 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>g', group = '[G]it' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
