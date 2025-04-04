3170432795c59228b5723dc03e0390e7e15cb3ea
Iñigo Solano <i@errstate.com>
2025-04-04 08:18:38 +0200

    tweak gitsigns

--- init.lua
+++ init.lua
@@ -277,12 +277,13 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        -- add = { text = '+' },
+        -- change = { text = '~' },
+        -- delete = { text = '_' },
+        -- topdelete = { text = '‾' },
+        -- changedelete = { text = '~' },
       },
+      current_line_blame = true,
     },
   },
 
